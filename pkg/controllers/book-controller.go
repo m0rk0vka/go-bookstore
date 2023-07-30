7b11f59ed383d788a52f86fad07c5e15e74d38a6
t.vladimir <vovatatosyan2000@mail.ru>
2023-07-30 16:48:10 +0300

    Added delete func in models

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -46,3 +46,18 @@ func CreateBook(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	Id, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Printf("error while parsing id")
+	}
+
+	deletedBook := models.DeleteBook(Id)
+	res, _ := json.Marshal(deletedBook)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}

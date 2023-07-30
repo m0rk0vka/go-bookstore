62bf744c4fa460af65ced00913314341f6c6402c
t.vladimir <vovatatosyan2000@mail.ru>
2023-07-30 16:29:39 +0300

    Added get book by id func

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -2,9 +2,12 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
+	"strconv"
 
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/m0rk0vka/go-bookstore/pkg/models"
 )
 
@@ -13,6 +16,21 @@ var NewBook models.Book
 func GetBooks(w http.ResponseWriter, r *http.Request) {
 	newBooks := models.GetAllBooks()
 	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	Id, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Printf("error while parsing id\n")
+	}
+
+	bookDetails, _ := models.GetBookById(Id)
+	res, _ := json.Marshal(bookDetails)
 	w.Header().Set("Content-Type", "pkglication/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
